93a672ab54852d7432ba0d0e8304081670640259
Abhishek M <abhi.24.2002@gmail.com>
2023-10-20 19:04:24 +0530

    2nd

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', function () {
-    const uploadForm = document.getElementById('upload-form');
     const imageUpload = document.getElementById('image-upload');
     const processedImage = document.getElementById('processed-image');
+    const removeBackgroundButton = document.getElementById('remove-background-button');
     const downloadButton = document.getElementById('download-button');
 
-    uploadForm.addEventListener('submit', async function (e) {
-        e.preventDefault();
-
-        processedImage.src = 'loading.gif';
-
+    removeBackgroundButton.addEventListener('click', function () {
         const file = imageUpload.files[0];
 
         if (file) {
-            const formData = new FormData();
-            formData.append('image', file);
+            const reader = new FileReader();
+            reader.onload = function () {
+                const imageData = new Image();
+                imageData.src = reader.result;
 
-            try {
-                const response = await fetch('/remove-background', {
-                    method: 'POST',
-                    body: formData,
-                });
+                imageData.onload = function () {
+                    const canvas = document.createElement('canvas');
+                    canvas.width = imageData.width;
+                    canvas.height = imageData.height;
+                    const ctx = canvas.getContext('2d');
+                    ctx.drawImage(imageData, 0, 0);
 
-                if (response.ok) {
-                    const result = await response.json();
-                    processedImage.src = result.processedImageURL;
-                    downloadButton.href = result.processedImageURL;
-                    downloadButton.style.display = 'block';
-                } else {
-                    processedImage.src = 'error.png';
-                }
-            } catch (error) {
-                console.error(error);
-                processedImage.src = 'error.png';
-            }
-        } else {
-            processedImage.src = '';
+                    rembg(canvas).then((result) => {
+                        ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        processedImage.width = canvas.width;
+                        processedImage.height = canvas.height;
+                        const processedCtx = processedImage.getContext('2d');
+                        processedCtx.drawImage(result, 0, 0);
+                        downloadButton.style.display = 'block';
+                    });
+                };
+            };
+            reader.readAsDataURL(file);
         }
+    });
+
+    downloadButton.addEventListener('click', function () {
+        const imgData = processedImage.toDataURL('image/png');
+        const a = document.createElement('a');
+        a.href = imgData;
+        a.download = 'processed-image.png';
+        a.click();
     });
 });
